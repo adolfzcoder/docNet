4e47bfb4170c8341cbc2d368a75b326d2bc751a3
Rejoice-t <rejoice.teca@gmail.com>
2025-05-05 19:39:32 +0200

    controllers added

---
+++ src/UIandControllers/loginController.java
@@ -0,0 +1,32 @@
+package UIandControllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class loginController {
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private Button login;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private Button signup;
+
+    @FXML
+    void logoutButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signUpButtonClicked(ActionEvent event) {
+
+    }
+
+}
---
+++ src/UIandControllers/searchDoctorController.java
@@ -0,0 +1,65 @@
+package UIandControllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class searchDoctorController {
+
+    @FXML
+    private Button bookButton;
+
+    @FXML
+    private Text dash;
+
+    @FXML
+    private ScrollBar docList;
+
+    @FXML
+    private Text docName;
+
+    @FXML
+    private Text findDoc;
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    private Text operating;
+
+    @FXML
+    private Text rate;
+
+    @FXML
+    private ImageView searchButton;
+
+    @FXML
+    private TextField searchbar;
+
+    @FXML
+    private Text spec;
+
+    @FXML
+    private Text userHandle;
+
+    @FXML
+    void bookButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void findDoc(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logoutButtonClicked(ActionEvent event) {
+
+    }
+
+}
