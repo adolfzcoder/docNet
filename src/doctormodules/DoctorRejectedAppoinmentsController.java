0af004c451af4c95e5b0bf85a3934405f4a89987
Rejoice-t <rejoice.teca@gmail.com>
2025-05-08 20:52:10 +0200

    added new  pages for rejected and approved appointment

---
+++ src/doctormodules/DoctorRejectedAppoinmentsController.java
@@ -0,0 +1,36 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class DoctorRejectedAppoinmentsController {
+
+    @FXML
+    private Label dynamicTotalOrPendingOrApprovedOrDeclinedAppointment;
+
+    @FXML
+    void appointmentsRedirectClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void dashboardRedirectClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logoutButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void manageDoctorsRedirectClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void profileRedirectClicked(MouseEvent event) {
+
+    }
+
+}
